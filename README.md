53c42dc78363a65c13ed156ec8937cdce0b60b21
Duc-Canh <canhnd.vn@gmail.com>
2019-03-28 14:04:07 +0100

    Trigger rebuild

---
+++ README.md
@@ -0,0 +1,1 @@
+# NguyenDucCanh.github.io
---
+++ jemdoc.sh
@@ -0,0 +1,3 @@
+#python jemdoc.py -c bibtex.conf publications.jemdoc
+#python jemdoc.py index.jemdoc publications.jemdoc media_awards.jemdoc
+python jemdoc.py publications.jemdoc
